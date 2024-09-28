6d226c86e3986193167c745be0ff2309cbe902e9
seoheev <pp123qq@dgu.ac.kr>
2024-09-29 03:31:11 +0900

    2024-09-29 개인과제6 5번

--- Prac5/hello.py
+++ Prac5/hello.py
@@ -1,8 +1,10 @@
 name = input("이름을 입력하세요: ")
 number = input("학번을 입력하세요: ")
 department = input("학과를 입력하세요: ")
+grade = input("학년을 입력하세요: ")
 
 print(f"""\n<출력>
 이름: {name}
 학번: {number}
-학과: {department}""")
+학과: {department}
+학년: {grade}""")
